80bd458d3973e569ece42a11e2179c47ec55bea8
Josh Winton <joshcwinton@gmail.com>
2020-05-16 18:48:00 -0400

    tried using findfundmat -> stereorectify -> warperspec

--- main.py
+++ main.py
@@ -74,14 +74,8 @@ def paper_2013(img_l, img_r, min_keypoints, min_matches):
     # Borrowed from feature matching tutorial
     # Match keypoints - may want to change this to FLANN
     bfm = cv.BFMatcher(cv.NORM_HAMMING)
-    # matches = bfm.match(desc_l, desc_r)
     matches = bfm.knnMatch(desc_l, desc_r, k=2)
-    # matches = sorted(matches, key=lambda x: x.distance)
     print("Number of matches: ", len(matches))
-    # img_matches = cv.drawMatches(img_l, keypoints_l, img_r, keypoints_r,
-    #  matches[:1000], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    # cv.imshow("Matches", img_matches)
-    # cv.waitKey(0)
 
     # Get fundamental matrix
     good_matches = []
@@ -89,10 +83,6 @@ def paper_2013(img_l, img_r, min_keypoints, min_matches):
         if m.distance < 0.68*n.distance:
             good_matches.append(m)
 
-    print(len(good_matches))
-    img_matches = cv.drawMatches(img_l, keypoints_l, img_r, keypoints_r,
-                                 good_matches, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    cv.imshow("Good Matches", img_matches)
     if len(good_matches) > min_matches:
         src_pts = np.float32(
             [keypoints_l[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
@@ -100,39 +90,20 @@ def paper_2013(img_l, img_r, min_keypoints, min_matches):
             [keypoints_r[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
         fund_mat, mask = cv.findFundamentalMat(src_pts, dst_pts)
+
         res, H1, H2 = cv.stereoRectifyUncalibrated(
             src_pts, dst_pts, fund_mat, img_l.shape, 10)
+
         img_l_transformed = cv.warpPerspective(
             src=img_l, M=H1, dsize=(img_l.shape[1], img_l.shape[0]))
         img_r_transformed = cv.warpPerspective(
             src=img_r, M=H2, dsize=(img_l.shape[1], img_l.shape[0]))
-        # img_l_transformed=cv.warpPerspective(
-        #     img_l, matrix, (img_l.shape[1], img_l.shape[0]))
-        # src_pts = np.float32(
-        #     [keypoints_l[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-        # dst_pts = np.float32(
-        #     [keypoints_r[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-
-        # # May want to tune findFundamentalMat parameters
-        # fund_mat, _ = cv.findFundamentalMat(src_pts, dst_pts)
-        # H, _ = cv.findHomography(src_pts, dst_pts)
-        # ic(H)
-        # img_l_out = np.empty(img_l.shape, dtype=np.uint8)
 
         # fundamental = np.array([[float("6.06055943873370e-09"),	float("1.92223199610921e-06"), float("-0.000996332361126767")],
         #                        [float(
         #                            "-2.96185493980771e-06"), float("-9.80787000103649e-07"), float("0.174207730231353")],
         #                        [float("0.000903900940100796"), float("-0.172536484910197"), float("4.42844607271752")]], dtype=np.float32)
 
-        # #cv.WARP_INVERSE_MAP = True
-        # ic(img_l)
-        # ic(fundamental)
-        # #img_l_transformed = copy.deepcopy(img_l)
-        # img_l_transformed = np.empty((img_l.shape[1], img_l.shape[0]), dtype=np.uint8)
-        # img_l_transformed = cv.warpPerspective(src=img_l, M=H, dsize=(img_l.shape[1], img_l.shape[0]))
-        # ic(img_l_transformed)
-        # print(img_l.shape)
-        # print(img_l_transformed.shape)
         cv.imshow("Transformed LEFT", img_l_transformed)
         cv.imshow("Transformed RIGHT", img_r_transformed)
         cv.imshow("left", img_l)
