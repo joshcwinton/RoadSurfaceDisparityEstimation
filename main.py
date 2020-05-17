821a0e16c5884f96eeedf2ae62d3413947d85bf4
Daniel Mallia <Daniel.Mallia21@myhunter.cuny.edu>
2020-05-16 22:03:49 -0400

    Revise script and add 2013 arguments

--- main.py
+++ main.py
@@ -21,156 +21,163 @@ import copy
 
 
 def read_images(lfp, rfp):
-    img_l = cv.imread(lfp, 0)
-    img_r = cv.imread(rfp, 0)
+	img_l = cv.imread(lfp, 0)
+	img_r = cv.imread(rfp, 0)
 
-    if img_l is None:
-        sys.exit("Could not open LEFT image file.")
-    if img_r is None:
-        sys.exit("Could not open RIGHT image file.")
+	if img_l is None:
+		sys.exit("Could not open LEFT image file.")
+	if img_r is None:
+		sys.exit("Could not open RIGHT image file.")
 
-    if(img_l.shape == img_r.shape):
-        rows, cols = img_l.shape
-        print("Rows: ", rows, " Cols: ", cols)
-    else:
-        sys.exit()
+	if(img_l.shape == img_r.shape):
+		rows, cols = img_l.shape
+		print("Rows: ", rows, " Cols: ", cols)
+	else:
+		sys.exit()
 
-    return img_l, img_r
+	return img_l, img_r
 
 
 def view_keypoints(src, keypoints, orientation):
-    img_with_keypoints = np.empty(src.shape, dtype=np.uint8)
-    img_with_keypoints = cv.drawKeypoints(src, keypoints, img_with_keypoints)
-    # cv.imshow("Image " + orientation, img_with_keypoints)
-    cv.waitKey(0)
+	img_with_keypoints = np.empty(src.shape, dtype=np.uint8)
+	img_with_keypoints = cv.drawKeypoints(src, keypoints, img_with_keypoints)
+	# cv.imshow("Image " + orientation, img_with_keypoints)
+	cv.waitKey(0)
 
 
 def check_keypoints(keypoints, orientation, minimum):
-    num = len(keypoints)
-    print("Number of keypoints calculated for ", orientation, ": ", num)
-    if(num < minimum):
-        print("Minimum number of keypoints not met for image: ", orientation)
-        sys.exit()
+	num = len(keypoints)
+	print("Number of keypoints calculated for ", orientation, ": ", num)
+	if(num < minimum):
+		print("Minimum number of keypoints not met for image: ", orientation)
+		sys.exit()
 
 # 2013 Algorithm
 # Disparity calculation algorithm
 # Assumes rectified input images
-def paper_2013(img_l, img_r):
+def paper_2013(img_l, img_r, w, dmax, tau):
 
 
 def paper_2017():
-    pass
+	pass
 
 def paper_2018():
-    # Create detector and detect keypoints
-    # May want to specify parameters for BRISK
-    detector = cv.BRISK.create()
-    keypoints_l, desc_l = detector.detectAndCompute(img_l, None)
-    keypoints_r, desc_r = detector.detectAndCompute(img_r, None)
+	# Create detector and detect keypoints
+	# May want to specify parameters for BRISK
+	detector = cv.BRISK.create()
+	keypoints_l, desc_l = detector.detectAndCompute(img_l, None)
+	keypoints_r, desc_r = detector.detectAndCompute(img_r, None)
 
-    # Print and check keypoint numbers
-    check_keypoints(keypoints_l, "LEFT", min_keypoints)
-    check_keypoints(keypoints_r, "RIGHT", min_keypoints)
+	# Print and check keypoint numbers
+	check_keypoints(keypoints_l, "LEFT", min_keypoints)
+	check_keypoints(keypoints_r, "RIGHT", min_keypoints)
 
-    # Test view keypoints
-    view_keypoints(img_l, keypoints_l, "LEFT")
-    view_keypoints(img_r, keypoints_r, "RIGHT")
+	# Test view keypoints
+	view_keypoints(img_l, keypoints_l, "LEFT")
+	view_keypoints(img_r, keypoints_r, "RIGHT")
 
-    # Borrowed from feature matching tutorial
-    # Match keypoints - may want to change this to FLANN
-    bfm = cv.BFMatcher(cv.NORM_HAMMING)
-    matches = bfm.match(desc_l, desc_r)
-    # matches = bfm.knnMatch(desc_l, desc_r, k=2)
-    # ic(matches)
-    matches = sorted(matches, key=lambda x: x.distance)
-    print("Number of matches: ", len(matches))
-    img_matches = cv.drawMatches(img_l, keypoints_l, img_r, keypoints_r,
-                                 matches[:1000], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    cv.imshow("Matches", img_matches)
-    # cv.waitKey(0)
+	# Borrowed from feature matching tutorial
+	# Match keypoints - may want to change this to FLANN
+	bfm = cv.BFMatcher(cv.NORM_HAMMING)
+	matches = bfm.match(desc_l, desc_r)
+	# matches = bfm.knnMatch(desc_l, desc_r, k=2)
+	# ic(matches)
+	matches = sorted(matches, key=lambda x: x.distance)
+	print("Number of matches: ", len(matches))
+	img_matches = cv.drawMatches(img_l, keypoints_l, img_r, keypoints_r,
+									matches[:1000], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+	cv.imshow("Matches", img_matches)
+	# cv.waitKey(0)
 
-    # Get fundamental matrix
-    good_matches = []
-    # for m, n in matches:
-    #     if m.distance < 0.75*n.distance:
-    #         good_matches.append(m)
-    for m in matches:
-        # print(m.distance)
-        if m.distance < 1000:
-            good_matches.append(m)
+	# Get fundamental matrix
+	good_matches = []
+	# for m, n in matches:
+	#     if m.distance < 0.75*n.distance:
+	#         good_matches.append(m)
+	for m in matches:
+		# print(m.distance)
+		if m.distance < 1000:
+			good_matches.append(m)
 
-    if len(good_matches) > min_matches:
-        src_pts = []
-        dst_pts = []
-        for i in range(4):
-            src_pts.append(list(keypoints_l[good_matches[i].queryIdx].pt))
-            dst_pts.append(list(keypoints_r[good_matches[i].trainIdx].pt))
+	if len(good_matches) > min_matches:
+		src_pts = []
+		dst_pts = []
+		for i in range(4):
+			src_pts.append(list(keypoints_l[good_matches[i].queryIdx].pt))
+			dst_pts.append(list(keypoints_r[good_matches[i].trainIdx].pt))
 
-        src_pts = np.float32(src_pts)
-        dst_pts = np.float32(dst_pts)
+		src_pts = np.float32(src_pts)
+		dst_pts = np.float32(dst_pts)
 
-        ic(type(src_pts))
-        ic(src_pts)
+		ic(type(src_pts))
+		ic(src_pts)
 
 		# topLeftDest = [0, 0]
 		# dest = np.float32([topLeftDest,bottomLeftDest, topRightDest, \
 		#         bottomRightDest])
 
-        matrix = cv.getPerspectiveTransform(src_pts, dst_pts)
-        img_l_transformed = cv.warpPerspective(
-            img_l, matrix, (img_l.shape[1], img_l.shape[0]))
-        # src_pts = np.float32(
-        #     [keypoints_l[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-        # dst_pts = np.float32(
-        #     [keypoints_r[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+		matrix = cv.getPerspectiveTransform(src_pts, dst_pts)
+		img_l_transformed = cv.warpPerspective(
+			img_l, matrix, (img_l.shape[1], img_l.shape[0]))
+		# src_pts = np.float32(
+		#     [keypoints_l[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+		# dst_pts = np.float32(
+		#     [keypoints_r[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
-        # # May want to tune findFundamentalMat parameters
-        # fund_mat, _ = cv.findFundamentalMat(src_pts, dst_pts)
-        # H, _ = cv.findHomography(src_pts, dst_pts)
-        # ic(H)
-        # img_l_out = np.empty(img_l.shape, dtype=np.uint8)
+		# # May want to tune findFundamentalMat parameters
+		# fund_mat, _ = cv.findFundamentalMat(src_pts, dst_pts)
+		# H, _ = cv.findHomography(src_pts, dst_pts)
+		# ic(H)
+		# img_l_out = np.empty(img_l.shape, dtype=np.uint8)
 
-        # fundamental = np.array([[float("6.06055943873370e-09"),	float("1.92223199610921e-06"), float("-0.000996332361126767")],
-        #                        [float(
-        #                            "-2.96185493980771e-06"), float("-9.80787000103649e-07"), float("0.174207730231353")],
-        #                        [float("0.000903900940100796"), float("-0.172536484910197"), float("4.42844607271752")]], dtype=np.float32)
+		# fundamental = np.array([[float("6.06055943873370e-09"),	float("1.92223199610921e-06"), float("-0.000996332361126767")],
+		#                        [float(
+		#                            "-2.96185493980771e-06"), float("-9.80787000103649e-07"), float("0.174207730231353")],
+		#                        [float("0.000903900940100796"), float("-0.172536484910197"), float("4.42844607271752")]], dtype=np.float32)
 
-        # #cv.WARP_INVERSE_MAP = True
-        # ic(img_l)
-        # ic(fundamental)
-        # #img_l_transformed = copy.deepcopy(img_l)
-        # img_l_transformed = np.empty((img_l.shape[1], img_l.shape[0]), dtype=np.uint8)
-        # img_l_transformed = cv.warpPerspective(src=img_l, M=H, dsize=(img_l.shape[1], img_l.shape[0]))
-        # ic(img_l_transformed)
-        # print(img_l.shape)
-        # print(img_l_transformed.shape)
-        cv.imshow("Transformed LEFT", img_l_transformed)
-        cv.imshow("left", img_l)
-        cv.imshow("right", img_r)
-        cv.waitKey(0)
+		# #cv.WARP_INVERSE_MAP = True
+		# ic(img_l)
+		# ic(fundamental)
+		# #img_l_transformed = copy.deepcopy(img_l)
+		# img_l_transformed = np.empty((img_l.shape[1], img_l.shape[0]), dtype=np.uint8)
+		# img_l_transformed = cv.warpPerspective(src=img_l, M=H, dsize=(img_l.shape[1], img_l.shape[0]))
+		# ic(img_l_transformed)
+		# print(img_l.shape)
+		# print(img_l_transformed.shape)
+		cv.imshow("Transformed LEFT", img_l_transformed)
+		cv.imshow("left", img_l)
+		cv.imshow("right", img_r)
+		cv.waitKey(0)
 
 
 def main():
-    # Parse arguments
-    parser = argparse.ArgumentParser("Disparity Map Estimation")
-    parser.add_argument("--left_img", type=str, required=True,
-                        help="Specify the filepath of the left image.")
-    parser.add_argument("--right_img", type=str, required=True,
-                        help="Specify the filepath of the right image.")
-    parser.add_argument("--paper", type=str, required=True,
-                        help="Specify the paper implementation.")
-    parser.add_argument("--min_keypoints", type=int, required=False,
-                        default=100, help="Specify the minimum number of keypoints")
-    parser.add_argument("--min_matches", type=int, required=False, default=10,
-                        help="Specify the minimum number of matches to proceed with 8-point\
-			algorithm")
-    args = parser.parse_args()
+	# Parse arguments
+	parser = argparse.ArgumentParser("Disparity Map Estimation")
+	parser.add_argument("--left_img", type=str, required=True,
+		help="Specify the filepath of the left image.")
+	parser.add_argument("--right_img", type=str, required=True,
+		help="Specify the filepath of the right image.")
+	parser.add_argument("--paper", type=str, required=True,
+		help="Specify the paper implementation.")
+	parser.add_argument("--min_keypoints", type=int, required=False,
+		default=100, help="Specify the minimum number of keypoints")
+	parser.add_argument("--min_matches", type=int, required=False, default=10,
+		help="Specify the minimum number of matches to proceed with 8-point\
+		algorithm")
+	parser.add_argument("--w", type=int, required=False, default=5,
+		help="Specify the window size.")
+	parser.add_argument("--dmax", type=int, required=False, default=100,
+		help="Specify the maximum disparity search range.")
+	parser.add_argument("--tau", type=int, required=False, default=2,
+		help="Specify the tau (tolerance).")
+	args = parser.parse_args()
 
-    # Read in images
-    img_l, img_r = read_images(args.left_img, args.right_img)
+	# Read in images
+	img_l, img_r = read_images(args.left_img, args.right_img)
 
-    # 2013 paper
-    paper_2013(img_l, img_r)
+	# 2013 paper
+	if(args.paper == "2013")
+		paper_2013(img_l, img_r, args.w, args.dmax, args.tau)
 
 
 if __name__ == "__main__":
