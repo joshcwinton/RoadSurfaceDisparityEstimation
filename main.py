f8c9def6a28f6bfab16d6ae601841d740df3e89c
joshcwinton <joshcwinton@gmail.com>
2020-05-21 00:58:39 -0400

    parens + commented some prints

--- main.py
+++ main.py
@@ -345,10 +345,9 @@ def paper_2017(img_l, img_r, w, d_max, tau, disp_thresh):
     #         µ_map_left[i][j] = (
     #             (µ_map_left[i][j] - min_µ) / (max_µ - min_µ)) * 255
 
-
     print("writing mu map")
     cv.imwrite("left_mu_map.png", µ_map_left)
-    
+
     ################ RIGHT µ MAP CALCULATION ################
     µ_map_right = np.zeros(int_img_r.shape, dtype=np.float32)
     rho = int((w-1)/2)
@@ -360,11 +359,11 @@ def paper_2017(img_l, img_r, w, d_max, tau, disp_thresh):
 
     for v in range(m - rho):
         for u in range(n - rho):
-            r1 = int_img_r[v+rho,u+rho]
-            r2 = int_img_r[v-rho-1, u-rho-1] 
-            r3 = int_img_r[v+rho, u-rho-1] 
+            r1 = int_img_r[v+rho, u+rho]
+            r2 = int_img_r[v-rho-1, u-rho-1]
+            r3 = int_img_r[v+rho, u-rho-1]
             r4 = int_img_r[v-rho-1, u+rho]
-            µ_map_right[v][u] = r1 + r2 - r3 - r4 / (w ** 2)
+            µ_map_right[v][u] = (r1 + r2 - r3 - r4) / (w ** 2)
             max_µ = max(max_µ, µ_map_right[v][u])
             min_µ = min(min_µ, µ_map_right[v][u])
 
@@ -372,10 +371,10 @@ def paper_2017(img_l, img_r, w, d_max, tau, disp_thresh):
 
     for i in range(len(µ_map_right)):
         for j in range(len(µ_map_right[0])):
-            µ_map_right[i][j] = ((µ_map_right[i][j] - min_µ) / (max_µ - min_µ)) * 255
+            µ_map_right[i][j] = (
+                (µ_map_right[i][j] - min_µ) / (max_µ - min_µ)) * 255
 
     cv.imwrite("right_mu_map.png", µ_map_right)
-
 
     ########## Left Sigma Calculation ###########
     sigma_map_left = np.zeros(int_img_l.shape, dtype=np.float32)
@@ -423,13 +422,13 @@ def paper_2017(img_l, img_r, w, d_max, tau, disp_thresh):
     #         sigma_map_right[i][j] = ((sigma_map_right[i][j] - min_sigma) / (max_sigma - min_sigma)) * 255
 
     # cv.imwrite("sigma_map.png", sigma_map_right)
-    
+
     # v = m - rho - 2
-    # for u_l in range(rho + d_max+1, n-rho-d_max-1): 
+    # for u_l in range(rho + d_max+1, n-rho-d_max-1):
     #     for d in range(0, 71):
     #         u_r = u_l - d
-    #         if 
-            
+    #         if
+
 
 def paper_2018():
     # Create detector and detect keypoints
